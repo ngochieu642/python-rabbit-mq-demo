a9f458f02f5f5a9e9c426a175c37dbe9ae35958d
coder <ngochieu10041997@gmail.com>
2020-02-10 17:07:25 +0700

    WIP

--- send.py
+++ send.py
@@ -1,7 +1,9 @@
 import pika
+import json
+import constant
 
 
-def sendRabbitMQ():
+def sendRabbitMQ(body):
     credentials = pika.PlainCredentials('rabbit', 'rabbitpass')
     paramaters = pika.ConnectionParameters(host='localhost',
                                            port=5672,
@@ -27,11 +29,11 @@ def sendRabbitMQ():
 
     try:
         channel.basic_publish(exchange='amq.topic',
-                            routing_key='update.info.gateway.hieuTest',
-                            body='Hello',
-                            properties=pika.BasicProperties(
-                                content_type='text/plain', delivery_mode=1),
-                            mandatory=True)
+                              routing_key='update.info.gateway.hieuTest',
+                              body=json.dumps(body),
+                              properties=pika.BasicProperties(
+                                  content_type='text/plain', delivery_mode=1),
+                              mandatory=True)
     except pika.exceptions.UnroutableError as e:
         print('Unroutable')
         print(e)
@@ -50,4 +52,25 @@ def str2byte(inputString):
     data = inputString.encode('utf-8')
     return data
 
-sendRabbitMQ()
+
+class ReceiveMonitor():
+    def __init__(self, msgType, status, msg, routingKey):
+        self.msgType = msgType
+        self.status = status
+        self.msg = msg
+        self.routingKey = routingKey
+
+    def getJSON(self):
+        return {
+            "type": constant.ANSWER_TYPE,
+            "payload": {
+                "type": self.type,
+                "status": self.status,
+                "msg": self.msg
+            },
+            "routingKey": self.routingKey
+            # "monitor.result.cli_93f70840-719e-4a79-bf74-e51a6e1c9708"
+        }
+
+
+constant.ANSWER_PAYLOAD_STATUS.get('SUCCESS')
